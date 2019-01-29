ad2563f7b974dfddcbeef7c7005b662d992a1752
Kyle Sanders <kyle.sanders.315@gmail.com>
2019-01-29 06:49:23 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -11,6 +11,6 @@ def input_to_index(input)
   input.to_i - 1
 end
 
-def move(board_array, board_index, char = "X")
-  board_array[board_index] = char
+def move(board, index, char = "X")
+  board[index] = char
 end
